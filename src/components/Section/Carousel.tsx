a5539fd913edb74184306637cc8ce80fc2961fca
khurramayubi <khurramayubi@hotmail.com>
2025-01-19 23:22:26 -0500

    Update carousel style Add support to play video

--- src/components/Section/Carousel.tsx
+++ src/components/Section/Carousel.tsx
@@ -18,23 +18,34 @@ export default function SectionCard({ images }: { images: string[] }) {
 
 	return (
 		<div
-			className="carousel-container relative h-[500px] w-auto p-8 rounded-xl"
+			className="carousel-container h-[500px] w-auto p-8 rounded-xl flex flex-col gap-4 items-center"
 			style={{
 				backgroundColor: "#2a2a2a",
 				boxShadow: "0 4px 20px rgba(0, 0, 0, 0.5)"
 			}}
 		>
-			<div className="h-full w-auto">
-				<img
-					style={{ objectFit: "contain" }}
-					className="h-full w-auto justify-self-center"
-					src={images[currentIndex]}
-					alt={`Slide ${currentIndex + 1}`}
-				/>
+			<h2 className="text-yellow-500 font-bold text-2xl">Media</h2>
+			<div className="h-[80%] w-auto">
+				{images[currentIndex].indexOf(".mp4") > 0 ? (
+					<video
+						controls={true}
+						autoPlay={true}
+						src={images[currentIndex]}
+						style={{ objectFit: "contain" }}
+						className="h-full w-auto justify-self-center"
+					/>
+				) : (
+					<img
+						style={{ objectFit: "contain" }}
+						className="h-full w-auto justify-self-center"
+						src={images[currentIndex]}
+						alt={`Slide ${currentIndex + 1}`}
+					/>
+				)}
 			</div>
-			<div className="carousel-controls absolute bottom-8 right-12 flex gap-4 items-center">
+			<div className="carousel-controls flex gap-4 items-center justify-center">
 				<button
-					className="h-[50px] w-[50px] bg-yellow-500 text-[#1e1e1e] font-extrabold rounded-full hover:bg-yellow-400 active:bg-yellow-600 transition duration-300  disabled:bg-gray-400"
+					className="h-[35px] w-[35px] bg-yellow-500 text-[#1e1e1e] font-extrabold rounded-full hover:bg-yellow-400 active:bg-yellow-600 transition duration-300  disabled:bg-gray-400"
 					onClick={handlePrev}
 					aria-label="Previous Image"
 					disabled={images.length < 2}
@@ -43,7 +54,7 @@ export default function SectionCard({ images }: { images: string[] }) {
 				</button>
 				<p className="text-white">{`${currentIndex + 1} / ${images.length}`}</p>
 				<button
-					className="h-[50px] w-[50px] bg-yellow-500 text-[#1e1e1e] font-extrabold rounded-full hover:bg-yellow-400 active:bg-yellow-600 disabled:bg-gray-400 transition duration-300"
+					className="h-[35px] w-[35px] bg-yellow-500 text-[#1e1e1e] font-extrabold rounded-full hover:bg-yellow-400 active:bg-yellow-600 disabled:bg-gray-400 transition duration-300"
 					onClick={handleNext}
 					aria-label="Previous Image"
 					disabled={images.length < 2}
